67dd8d436a75eebd4475d928f2349f5ab3077914
NureKolodiiukNadiia <nadiia.kolodiiuk@nure.ua>
2024-10-23 18:50:25 +0300

    add pz1

---
+++ PractTask1/android-pzpi-23-2-kolodiiuk-nadiia-pract-task1/app/src/main/java/nadiia/kolodiiuk/practtask1/MainActivity.java
@@ -0,0 +1,36 @@
+package nadiia.kolodiiuk.practtask1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button myButton = findViewById(R.id.pressMeButton);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = findViewById(R.id.text);
+                textView.setText("Натиснуто");
+            }
+        });
+    }
+}
