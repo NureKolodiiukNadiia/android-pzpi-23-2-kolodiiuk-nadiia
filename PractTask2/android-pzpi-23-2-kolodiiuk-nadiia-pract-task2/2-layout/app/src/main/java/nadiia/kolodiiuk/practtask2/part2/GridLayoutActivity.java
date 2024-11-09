2494655fbb3f3df54ef8426b2b4033bfbda511c6
NureKolodiiukNadiia <nadiia.kolodiiuk@nure.ua>
2024-11-09 12:04:01 +0200

    add pract task 2

---
+++ PractTask2/android-pzpi-23-2-kolodiiuk-nadiia-pract-task2/2-layout/app/src/main/java/nadiia/kolodiiuk/practtask2/part2/GridLayoutActivity.java
@@ -0,0 +1,38 @@
+package nadiia.kolodiiuk.practtask2.part2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class GridLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_grid_layout);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void changeText(View view) {
+
+        TextView textView1 = findViewById(R.id.textView1);
+        textView1.setText("Текст змінено");
+    }
+
+    public void showMessage(View view) {
+
+        Toast.makeText(this, "Повідомлення", Toast.LENGTH_SHORT).show();
+    }
+}
